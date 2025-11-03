ae4ff49e754716ad96875a7aedbcd8f7c54e817a
Bandish Kumar <bkumar@fastenal.com>
2025-11-03 17:14:05 +0530

    Initialize the app with Xcode

---
+++ ObjectIdentifyingApp/ObjectIdentifyingApp/ObjectIdentifyingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectIdentifyingAppApp.swift
+//  ObjectIdentifyingApp
+//
+//  Created by Bandish Kumar on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ObjectIdentifyingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
